069a80f9d2486f5f0f8d8b75740f4c59c5eee671
udi <udi@udis-MacBook-Pro.local>
2016-12-13 15:07:36 -0800

    Creating test bdb function

--- dgadb.c
+++ dgadb.c
@@ -1,11 +1,27 @@
+/*
+  dga database interface, this function will create the database if its not exist
+  cache and return values all ready cached
+.
+*/
+
 #include <stdio.h>
 #include <db.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
+
 #include "global.h"
 
-int main(){
+int main(int argc, char *argv[]){
   DB *dbp; /* DB structure handle */
+  DBT key, data;
+
+  char *keyname = argv[1], *verdict = argv[2];
+  
+  // initialize key, data
+  memset(&data, 0, sizeof(DBT));
+  memset(&key, 0, sizeof(DBT));
+
   u_int32_t flags; /* database open flags */
   int ret; /* function return value */
 
@@ -31,13 +47,44 @@ int main(){
                  flags,
                  0);
   if(ret != 0){ 
-   perror("open database failed");
-   exit(EXIT_FAILURE);
+   dbp->err(dbp, 0, "dbp-open");
+   return EXIT_FAILURE;
   }
 
+ /* this is for test only, pointing argv as the key, value
+    the key is an array
+ */
 
+  key.data = keyname;
+  key.size = strlen(keyname) + 1;
+
+  data.data = verdict;
+  data.size = strlen(verdict) + 1;
+
+  // trying to retrieve the data
+  if((ret = dbp->get(dbp, NULL, &key, &data, 0)) == 0){
+    printf("we have hit !!!, key:%s value:%s\n", (char *)key.data, (char *)data.data);
+    // data extracted, we can safely close the db
+    if(dbp != NULL){
+      dbp->close(dbp, 0);
+    }
+
+  // return data value 
+  return atoi(argv[2]);
+
+  }else{
+    // we have a miss !!!, adding values //
+    dbp->err(dbp, ret, "DB->get");
+    printf("adding data\n");
+    if((ret = dbp->put(dbp, NULL, &key, &data, 0)) == 0){
+     printf("caching the data, db was updated\n");
+    }else{
+      dbp->err(dbp, ret, "DB->put");
+    }
+  }
+  
   /* When we're done with the database, close it. */
-if (dbp != NULL)
- dbp->close(dbp, 0);
-}
-                 
+  if (dbp != NULL){
+   dbp->close(dbp, 0);
+  }
+}                
